5aae65e9b470ae2c18a0807b800da51cd5410eaa
renugopal <renugopal17@hotmail.com>
2025-01-21 07:20:47 -0600

    Time: 765 ms (56.9%), Space: 18.1 MB (54.98%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,15 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # we need to check for using each coin everytime until the target is reached
-        #for every penny we try every coin
-        # dp soln.
-        dp = [amount + 1] * (amount + 1)
+        # fewest no of coins to make up that amount
+        # something like greedy
+        # try to use more of higher denomincation coins
+        # create a dp cache - 0 o target
+        # dp[0] = 0 no of coins needed to make target = 0
+        # initialize rest all to infy
+        # for each denomination if i - denom >= 0 then 
+        # dp[i] = min(dp[i], 1 + dp[i-denom])
+        # what if multiple coins can do it (of the same denom)?
+        # we can optimize in future
+        dp = [amount + 1 for _ in range(amount + 1)]
+        # freq  can never be amount +1 , for coun == 1, amount is freq
         dp[0] = 0
 
-        for amt in range(1, amount+1):
-            for denomination in coins:
-                if amt - denomination >= 0:
-                    dp[amt] = min(dp[amt], 1 + dp[amt - denomination])
-        return dp[amount] if dp[amount] != amount + 1 else -1
-
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i],  1 + dp[i - coin])
         
+        return dp[-1] if dp[-1] != amount + 1 else -1
+
+
