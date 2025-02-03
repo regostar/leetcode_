0c94147e2c36178ca43a74ba07c3b5fc0f1fe97c
renugopal <renugopal17@hotmail.com>
2025-02-02 22:01:22 -0600

    Time: 1 ms (85.84%), Space: 19.8 MB (28.63%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,25 @@
+# # Definition of the Interval class
+# class Interval(object):
+#     def __init__(self, start, end):
+#         self.start = start
+#         self.end = end
+
+# The Solution class with the canAttendMeetings method
+class Solution:
+    def canAttendMeetings(self, intervals):
+        
+        if not intervals:
+            return True
+        intervals.sort()  # Sort intervals by start time
+
+        # check for overlapping intervals
+        prev_last = intervals[0][1]
+        for start, last in intervals[1:]:
+            # print(start)
+            if start < prev_last:
+                return False
+            else:
+                prev_last = last
+                # always last > start
+                # wkt start > prev_last so no need to find min no overlap
+        return True
