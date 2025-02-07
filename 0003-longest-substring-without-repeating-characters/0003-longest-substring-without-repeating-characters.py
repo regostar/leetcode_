d692634732c6b727ba8081364f886f1bde03e0a4
renugopal <renugopal17@hotmail.com>
2025-02-07 11:57:53 -0600

    Time: 20 ms (41.17%), Space: 18 MB (29.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,34 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # we will use a sliding window and a hash for the chars inside the window
-        # start from left
-        left, right = 0, 0
-        max_len = 0
-        cur_hash = set()
-        for right in range(len(s)):
-            # print(cur_hash)
-            if s[right] not in cur_hash:
-                # until there is no repitition, add right
-                max_len = max(max_len, right - left + 1)
-            else:
-                #pop left
-                while s[right] in cur_hash:
-                    # somewhere in left we have a repitition
-                    cur_hash.remove(s[left])
-                    left += 1
-                    # print("before add ",cur_hash)
-            cur_hash.add(s[right])
-        return max_len
-                
+        n = len(s)
+        ans = 0
+        # charToNextIndex stores the index after current character
+        charToNextIndex = {}
 
-            
-            # if s[right] in cur_hash:
-            #     left = right
-            #     cur_hash = set()
-            
-            # cur_hash.add(s[right])
-            # print(cur_hash)
-            # max_len = max(max_len, len(cur_hash))
+        i = 0
+        # try to extend the range [i, j]
+        for j in range(n):
+            if s[j] in charToNextIndex:
+                i = max(charToNextIndex[s[j]], i)
+                # what if last seen is left of i
 
-            
-        
+            ans = max(ans, j - i + 1)
+            charToNextIndex[s[j]] = j + 1
+
+        return ans
