8ce6baf59edebde7adbcae38c66d74564ea2420a
renugopal <renugopal17@hotmail.com>
2025-02-15 19:54:07 -0600

    Time: 115 ms (57.69%), Space: 31.3 MB (14.62%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -12,8 +12,12 @@ class NumMatrix:
         # at query time, we do O(1)
         # cumulative sum wrt origin
         self.prefix_sum = [[0 for _ in range(COLS+1)] for _ in range(ROWS+1)]
+        # initialize one set of row col to 0 for ease of computation
+
         for r in range(ROWS):
             for c in range(COLS):
+                # above + left - topleft 
+                # because it was added twice while computing left and right in itself
                 self.prefix_sum[r+1][c+1] = self.prefix_sum[r+1][c] + self.prefix_sum[r][c+1] - self.prefix_sum[r][c] + matrix[r][c]
 
         
@@ -21,7 +25,8 @@ class NumMatrix:
         
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-
+        # our result is 1+
+        # same formula curr - left - top + topleft since it was computed already
         result = self.prefix_sum[row2+1][col2+1] + self.prefix_sum[row1][col1] - self.prefix_sum[row1][col2+1] - self.prefix_sum[row2+1][col1]
 
         return result
