db698f8f9fca185637671dd82b8d1ba0d5384cad
renugopal <renugopal17@hotmail.com>
2025-02-20 00:10:18 -0600

    Time: 0 ms (100%), Space: 17.8 MB (74.29%) - LeetHub

---
+++ 0161-one-edit-distance/0161-one-edit-distance.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+        # abcde
+        # abde
+        # len differ by more than 1 - false
+        # if len are equal, then one update 
+        # if len are unequal then insert in one or delete in other is same
+        s_len = len(s)
+        t_len = len(t)
+        if abs(s_len - t_len) > 1:
+            return False
+        
+        for i in range(min(s_len, t_len)):
+            if s[i] != t[i]:
+                if s_len == t_len:
+                    return s[i+1:] == t[i+1:]
+                elif s_len < t_len:
+                    return t[i+1:] == s[i:]
+                else:
+                    return s[i+1:] == t[i:]
+        
+        # length is same but not matching taken care
+        # length is different == 1 
+        return abs(s_len - t_len) == 1
+
+        
