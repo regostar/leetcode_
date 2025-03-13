9846d6b76ddbfb2d5f2f6891b893d6589daeeddf
renugopal <renugopal17@hotmail.com>
2025-03-12 19:07:18 -0500

    Time: 27 ms (84.46%), Space: 20.3 MB (92.72%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,25 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        """
-         A key observation is that the sum of a subarray [i, j]
-         is equal to the sum of [0, j] minus the sum of [0, i - 1].
-
-         we compute prefix sums and store in dict with index
-         we find curr sum - target in the dict, if yes we have found
-        """
-        prefix_sum = 0
+        prefix_sum = {0: 1}
         count = 0
-        prefix_sum_dict = {0: 1}
-        for i in range(len(nums)):
-            prefix_sum += nums[i]
-            if prefix_sum - k in prefix_sum_dict:
-                count += prefix_sum_dict[prefix_sum - k]
-                # return prefix_sum_dict[prefix_sum - target] +1
-            prefix_sum_dict[prefix_sum] = prefix_sum_dict.get(prefix_sum, 0) + 1
-            # print(prefix_sum_dict)
+        cur_sum = 0
+        
+        for num in nums:
+            cur_sum += num
+            
+            if cur_sum - k in prefix_sum:
+                count += prefix_sum[cur_sum-k]
+            prefix_sum[cur_sum] = 1 + prefix_sum.get(cur_sum, 0)
         return count
-
-
-
-
+                
         
