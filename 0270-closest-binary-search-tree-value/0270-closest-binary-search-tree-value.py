7586d7b8a13a358577d464f531b947c4431759ff
renugopal <renugopal17@hotmail.com>
2025-04-11 00:28:29 -0500

    Time: 0 ms (100%), Space: 19.6 MB (40.18%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.py
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.py
@@ -14,25 +14,31 @@ class Solution:
         # we look at both it's children instead of just one 
         # we select one branch from that and traverse, based on the closest distance
         # we simply traverse full BST O(n) and find min diff ele
+        min_diff = float('inf')
         closest = root.val
-        def dfs(node):
+
+        def dfs(node: Optional[TreeNode]):
+            nonlocal min_diff
             nonlocal closest
             if not node:
+                # terminating condition
                 return
-            
+            if node.val == target:
+                # case  1
+                closest = node.val
             # Update the closest value if the current node is closer to the target
             if abs(node.val - target) < abs(closest - target) or \
             (abs(node.val - target) == abs(closest - target) and node.val < closest):
                 closest = node.val
             
             # Continue DFS in the left or right subtree based on the target value
+                        # Traverse only one branch based on the target value
             if target < node.val:
                 dfs(node.left)
-            else:
+            elif target > node.val:
                 dfs(node.right)
-        
-        # Start DFS from the root
+
+        # now if 2 has same min diff, we need to consider smallest
         dfs(root)
         return closest
-
 
