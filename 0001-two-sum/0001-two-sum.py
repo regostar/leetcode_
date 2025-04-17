2c75af1ca2d1dbee11dd741137a579dce5593b63
renugopal <renugopal17@hotmail.com>
2025-04-16 19:34:48 -0500

    Time: 3 ms (50.55%), Space: 19 MB (34.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_dict = {nums[index]: index for index in range(len(nums))}
-        result = []
+        # O(n) time and space using hash map
+        nums_dict = {nums[i]: i for i in range(len(nums))}
         print(nums_dict)
-        for j, each_num in enumerate(nums):
-            if target - each_num in nums_dict and  nums_dict[target - each_num] != j:
-                i = nums_dict[target - each_num]
-                result.append(min(i, j))
-                result.append(max(i, j))
-                break
-        return result
+        for i in range(len(nums)):
+
+            look_for_other = target - nums[i]
+            if look_for_other in nums_dict and nums_dict[look_for_other] != i:
+                return[i, nums_dict[look_for_other]]
+        
+
+
+        
