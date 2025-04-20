f72b671fbe51512f530e9412fe10f186854c7989
renugopal <renugopal17@hotmail.com>
2025-04-20 16:20:19 -0500

    Time: 0 ms (100%), Space: 18.9 MB (34.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # O(n) time and space using hash map
-        nums_dict = {nums[i]: i for i in range(len(nums))}
-        print(nums_dict)
-        for i in range(len(nums)):
+        # non unique elements are present
+        # 5,3,4,5,5,5
+        # t = 10
+        hashmap = {}
+        for i, ele in enumerate(nums):
+            compliment = target - ele
+            if compliment in hashmap:
+                return [i, hashmap[compliment]]
+            hashmap[ele] = i
+        return []
 
-            look_for_other = target - nums[i]
-            if look_for_other in nums_dict and nums_dict[look_for_other] != i:
-                return[i, nums_dict[look_for_other]]
         
 
 
