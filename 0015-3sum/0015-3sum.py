9c3020458a32646cbfff912084655d0036cc390f
renugopal <renugopal17@hotmail.com>
2025-04-20 17:39:57 -0500

    Time: 441 ms (89.68%), Space: 20.7 MB (38.67%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -23,6 +23,7 @@ class Solution:
                 else:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
+                    r -= 1
                     while l < r and nums[l] == nums[l - 1]:
                         l += 1
         return res
