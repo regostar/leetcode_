c03b01b23d17041d51c059c6ddb0a0a2e2ef92e0
renugopal <renugopal17@hotmail.com>
2025-04-21 16:36:14 -0500

    Time: 5 ms (7.12%), Space: 18.9 MB (96.24%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,24 +6,42 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # perform dfs
-        # count every node check max
-        # perform bfs - count no of max levels
+        stack = []
+        if root :
+            stack.append((1, root))
 
-        # recur4sive dfs
-        max_depth = 0
+        depth = 0
+        while stack:
+            cur_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, cur_depth)
+                stack.append((cur_depth + 1, root.left))
+                stack.append((cur_depth + 1, root.right))
+        return depth 
 
-        def dfs(node: Optional[TreeNode], depth_so_far: int =0):
-            if not node:
-                return
-            nonlocal max_depth
-            max_depth = max(max_depth, depth_so_far + 1)
-            dfs(node.left, depth_so_far + 1)
-            dfs(node.right, depth_so_far + 1)
+
+
+
+
+    # def maxDepth(self, root: Optional[TreeNode]) -> int:
+    #     # perform dfs
+    #     # count every node check max
+    #     # perform bfs - count no of max levels
+
+    #     # recur4sive dfs
+    #     max_depth = 0
+
+    #     def dfs(node: Optional[TreeNode], depth_so_far: int =0):
+    #         if not node:
+    #             return
+    #         nonlocal max_depth
+    #         max_depth = max(max_depth, depth_so_far + 1)
+    #         dfs(node.left, depth_so_far + 1)
+    #         dfs(node.right, depth_so_far + 1)
         
-        dfs(root, 0)
-        return max_depth
-        # Time complexity is O(N)
-        # Space complexity is O(N) - recursion call stack
-        # best case - tree is balanced O(log(N)).
+    #     dfs(root, 0)
+    #     return max_depth
+    #     # Time complexity is O(N)
+    #     # Space complexity is O(N) - recursion call stack
+    #     # best case - tree is balanced O(log(N)).
         
