61a91b899b86d578d3f160760ed08ad965e67e25
renugopal <renugopal17@hotmail.com>
2025-04-26 22:30:48 -0500

    Time: 73 ms (51.19%), Space: 23 MB (34.87%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,24 +1,19 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # min heap with k points
-        # return all k elements
-        # but how will we store the min heap -> [dist, x, y]
-        # can the result points be inm any order? - Yes
-        min_heap = []
-        for x, y in points:
-            dist = (x**2) + (y**2)
-            min_heap.append([dist, x, y])
-        
-        heapq.heapify(min_heap)
-        result = []
+        # creating a list for the min heap and adding all the distance values to it
+        minHeap = []
 
-        while k > 0:
-            dist, x, y = heapq.heappop(min_heap)
-            result.append([x, y])
+        def compute_dist(x, y):
+            return x**2 + y**2
+
+
+        dist_array = [(compute_dist(x, y), x, y) for x, y in points]
+        heapq.heapify(dist_array)
+
+        k_closest_points = []
+        while k:
+            dist, x, y = heapq.heappop(dist_array)
+            k_closest_points.append([x, y])
             k -= 1
-        
-        return result
 
-
-
-        
+        return k_closest_points
