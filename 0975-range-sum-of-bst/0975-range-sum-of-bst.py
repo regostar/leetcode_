f35841e40bcda9b0558514d8ae9545814534a882
renugopal <renugopal17@hotmail.com>
2025-04-26 20:44:29 -0500

    Time: 4 ms (48.22%), Space: 25.2 MB (58.29%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.py
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.py
@@ -8,20 +8,6 @@ class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         # consider as binary tree
         # simply do DFS
-        # ans = 0
-        # stack = [root]
-        # while stack:
-        #     node = stack.pop()
-        #     if node:
-        #         if low <= node.val <= high:
-        #             ans += node.val
-        #         # can we go to left subtree
-        #         # yes if node val is not lesser than low
-        #         if node.val > low:
-        #             stack.append(node.left)
-        #         if node.val < high:
-        #             stack.append(node.right)
-        #     return ans
         ans = 0
         stack = [root]
         while stack:
@@ -29,8 +15,11 @@ class Solution:
             if node:
                 if low <= node.val <= high:
                     ans += node.val
-                if low < node.val:
+                # can we go to left subtree
+                # yes if node val is not lesser than low
+                if node.val > low:
                     stack.append(node.left)
                 if node.val < high:
                     stack.append(node.right)
         return ans
+        
