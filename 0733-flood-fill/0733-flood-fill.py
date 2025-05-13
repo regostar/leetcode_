26505bdce43ac5aac0b4a499937dd9e109454e82
renugopal <renugopal17@hotmail.com>
2025-05-13 17:32:44 -0500

    Time: 0 ms (100%), Space: 18.1 MB (25.76%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,32 +1,32 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # graph BFS
         q = deque()
+        original_color = image[sr][sc]
         no_rows = len(image)
         no_cols = len(image[0])
-        original_color = image[sr][sc]
 
         if original_color == color:
-            return image  # Nothing to do
-
+            return image
+        
         def get_neighbors(row, col):
+            nonlocal no_rows
+            nonlocal no_cols
             directions = [(row-1, col), (row, col-1), (row+1, col), (row, col+1)]
             for r, c in directions:
-                if 0 <= r < no_rows and 0 <= c < no_cols:
-                    yield (r, c)
-
-        def bfs():
-            while q:
-                row, col = q.popleft()
-                image[row][col] = color  # Fill current
-
-                for nr, nc in get_neighbors(row, col):
-                    if image[nr][nc] == original_color:
-                        image[nr][nc] = color
-                        q.append((nr, nc))  # Add to queue
-
+                if r >= 0 and r <no_rows and c >=0 and c < no_cols:
+                    yield(r, c)
+        
         q.append((sr, sc))
-        bfs()
+        
+        while q:
+            row, col = q.popleft()
+
+            image[row][col] = color
+
+            for nr, nc in get_neighbors(row, col):
+                if image[nr][nc] == original_color:
+                    image[nr][nc] = color
+                    q.append((nr, nc))
         return image
+        
