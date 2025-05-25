c884315830b1b423e9a4ce7701db57f480befcc5
renugopal <renugopal17@hotmail.com>
2025-05-25 18:30:19 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -1,21 +1,44 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        left = 0
-        zeros = 0  # count of zeros in the current window
-        max_length = 0
+        # flip the 0s that lead to joining 2 chains of 1s
+        # thus creating a longer cons 1s chain
+        # 1 -> k = len(ar)
+        # return len(ar)
+        # 2 -> k = 0
+        # max cons 1s
+        # 3 -> k,  
+        # idea - count cons 1s  and 0s until no_0s == k,
+        # once 0s exceed k, new cons 1 should start, reset no_0s = 0
         
-        for right in range(len(nums)):
-            if nums[right] == 0:
-                zeros += 1
-            
-            # If there are more than k zeros in the window,
-            # move the left pointer until the window is valid again.
-            while zeros > k:
-                if nums[left] == 0:
-                    zeros -= 1
-                left += 1
-            
-            # Update the maximum window length found.
-            max_length = max(max_length, right - left + 1)
-        
-        return max_length
+        no_0s = 0
+        cons_1s = 0
+        max_cons_1s = 0
+        cons_flag = False
+
+        for i, num in enumerate(nums):
+            if not cons_flag:
+                if num == 0:
+                    no_0s += 1
+                    if no_0s > k:
+                        continue
+                cons_1s += 1
+                cons_flag = True
+            else:
+                if num == 0:
+                    no_0s += 1
+                    if no_0s > k:
+                        # reset
+                        no_0s = 0
+                        cons_1s = 0
+                        cons_flag = False
+                        continue
+                cons_1s += 1
+            max_cons_1s = max(max_cons_1s, cons_1s)
+
+        return  max_cons_1s
+                
+                
+                    
+
+
+
