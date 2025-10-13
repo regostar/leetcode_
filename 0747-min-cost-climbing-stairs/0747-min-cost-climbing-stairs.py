26eac8e9bdcc8ab219d3038db865ae6707c6ac3b
renugopal <renugopal17@hotmail.com>
2025-10-12 21:59:29 -0700

    Time: 3 ms (73.23%), Space: 17.8 MB (63.3%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,10 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        first = cost[0]
-        second = cost[1]
-        for i in range(2, n):
-            curr = cost[i] + min(first, second)
-            first = second
-            second = curr
-        return min(first, second)
+        # bottom up
+
+        # The array's length should be 1 longer than the length of cost
+        # This is because we can treat the "top floor" as a step to reach
+        min_cost = [0] * (len(cost) + 1)
+
+        # start from step 2
+        # cost for step 1 or 2 is 0
+        # 1 2 3 4 5
+        for i in range(2, len(cost) + 1):
+            take_one_step = min_cost[i-1] + cost[i-1]
+            take_two_steps = min_cost[i-2] + cost[i-2]
+            min_cost[i] = min(take_one_step, take_two_steps)
+        
+        return min_cost[-1]
