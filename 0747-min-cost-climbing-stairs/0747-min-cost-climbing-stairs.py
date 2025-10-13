cece9073a264fdf5e20f77a21d9065fe807339b1
renugopal <renugopal17@hotmail.com>
2025-10-12 22:17:26 -0700

    Time: 9 ms (7.47%), Space: 20.7 MB (7.2%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,23 +1,12 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # top down
-        # recursive
-        # end to start
-        # memoize the results computed
-        def recursive_find_min_cost(step: int)->int:
-            # base cases - 
-            if step <= 1:
+        @cache
+        def minimum_cost(i):
+            if i <= 1:
                 return 0
-            if step in results:
-                return results[step]
-            # recursive case -
-            min_cost_to_reach_n_1 = cost[step - 1] + recursive_find_min_cost(step - 1)
-            min_cost_to_reach_n_2 = cost[step - 2] + recursive_find_min_cost(step - 2)
-            results[step] = min(min_cost_to_reach_n_1, min_cost_to_reach_n_2)
-            return results[step] 
-        
-        results = {}
-        # can use arrays or dict
-        # arrays more efficient
-        n = len(cost)
-        return recursive_find_min_cost(n )
+            
+            down_one = cost[i - 1] + minimum_cost(i - 1)
+            down_two = cost[i - 2] + minimum_cost(i - 2)
+            return min(down_one, down_two)
+
+        return minimum_cost(len(cost))
